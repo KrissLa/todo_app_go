c597ef21c09e573024771520955e6f442ae98be0
Vitaliy Mishkov <vitaliy.mishkov@pm.by>
2021-12-12 23:29:06 +0300

    create todoList

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -12,11 +12,10 @@ type Authorization interface {
 }
 
 type TodoList interface {
-
+	Create(userId int, list todo.TodoList) (int, error)
 }
 
 type TodoItem interface {
-
 }
 
 type Service struct {
@@ -28,5 +27,6 @@ type Service struct {
 func NewService(repos *repository.Repository) *Service {
 	return &Service{
 		Authorization: NewAuthService(repos.Authorization),
+		TodoList:      NewTodoListService(repos.TodoList),
 	}
 }
